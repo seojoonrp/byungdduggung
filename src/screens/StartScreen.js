5bd05c4fd56aff7a4524feedababf9748b0e70d0
seojoonrp <seojoonrp@snu.ac.kr>
2025-01-26 19:29:10 +0900

    배경이미지 이상해서 다시 받아야됨

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 import './StartScreen.css';
 
+import BackgroundImage from '../images/BackgroundImage.svg';
+
 function StartScreen({ department, setDepartment }) {
   const navigate = useNavigate();
 
