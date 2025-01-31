875821f6ffcb73dc373008002a465968032edaf3
seojoonrp <seojoonrp@snu.ac.kr>
2025-01-31 20:34:57 +0900

    로직 코드는 대충 만듦

--- src/components/GamePanel.js
+++ src/components/GamePanel.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./GamePanel.css";
 
@@ -6,7 +6,68 @@ import PanelBorderImage from "../images/GameScreen/PanelBorderImage.svg";
 import BottleCapImage from "../images/GameScreen/BottleCapImage.svg"
 
 const GamePanel = () => {
-  const [curMode, setCurMode] = useState("Filling");
+  const capWidth = 75;
+  const totalLength = Math.PI * capWidth;
+
+  const fillSpeed = 0.003;
+  const rotationSpeed = 0.3;
+  const frameTime = 25;
+
+  const [curRatio, setCurRatio] = useState(0);
+  const [segmentRatio, setSegmentRatio] = useState(0);
+  const [confirmedRatio, setConfirmedRatio] = useState(0);
+  const [curRotation, setCurRotation] = useState(0);
+  const [curPivot, setCurPivot] = useState({ x: 200, y: 100 });
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  // 클릭 해제시 막대 채우기
+  useEffect(() => {
+    if (!isClicked && curRatio < 1) {
+      const fillInterval = setInterval(() => {
+        setCurRatio((prev) => Math.min(prev + fillSpeed, 1));
+      }, frameTime);
+
+      return () => clearInterval(fillInterval);
+    }
+  }, [isClicked, curRatio]);
+
+  // 클릭시 막대 돌리기
+  useEffect(() => {
+    if (isClicked) {
+      const rotateInterval = setInterval(() => {
+        setCurRotation((prev) => prev + rotationSpeed);
+      }, frameTime);
+
+      return () => clearInterval(rotateInterval);
+    }
+  }, [isClicked, curRotation]);
+
+  // 클릭했을 때
+  const handleMouseDown = () => {
+    setIsClicked(true);
+
+    setSegmentRatio(curRatio - confirmedRatio);
+    setConfirmedRatio(curRatio);
+  };
+
+  // 클릭 뗐을 때
+  const handleMouseUp = () => {
+    setIsClicked(false);
+
+    const newPivot = calculateNewPivot(confirmedRatio);
+    setCurPivot(newPivot);
+  };
+
+  const calculateNewPivot = () => {
+    const segmentLength = totalLength * segmentRatio;
+    const radRotation = curRotation * Math.PI / 180;
+
+    return {
+      x: curPivot.x + segmentLength * Math.sin(radRotation),
+      y: curPivot.y - segmentLength * Math.cos(radRotation)
+    }
+  }
 
   return (
     <div className="container">
@@ -14,6 +75,7 @@ const GamePanel = () => {
         src={BottleCapImage}
         alt="병뚜껑 이미지"
         className="bottle-cap-image"
+        style={{ width: `${capWidth}px` }}
       />
       <img
         src={PanelBorderImage}
