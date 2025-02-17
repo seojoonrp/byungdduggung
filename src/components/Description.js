841567425482aadbe5a10ae094304c84ec4c3099
seojoonrp <seojoonrp@snu.ac.kr>
2025-02-17 23:00:58 +0900

    유사도어케측정되나요 디자인 추가함

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,12 +4,8 @@ const Description = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="desc-overlay" onClick={onClose}>
-      <div className="desc" onClick={(e) => e.stopPropagation()}>
-        <button className="desc-close-button" onClick={onClose}>
-          ✕
-        </button>
-      </div>
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="desc" onClick={(e) => e.stopPropagation()} />
     </div>
   );
 };
--- src/components/HowCalculated.js
+++ src/components/HowCalculated.js
@@ -4,12 +4,11 @@ const HowCalculated = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="desc-overlay" onClick={onClose}>
-      <div className="desc" onClick={(e) => e.stopPropagation()}>
-        <button className="desc-close-button" onClick={onClose}>
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="how-calc" onClick={(e) => e.stopPropagation()}>
+        {/* <button className="modal-close-button" onClick={onClose}>
           ✕
-        </button>
-        <span>유사도는 주지매의 개쩌는 알고리즘으로 측정됩니다</span>
+        </button> */}
       </div>
     </div>
   );
