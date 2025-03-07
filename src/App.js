450c61086cd975cc9db0e4dc96034380db296f12
maximumjw <leex45774577@gmail.com>
2025-03-08 00:17:24 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './styles/fonts.css';
 import StartScreen from './screens/StartScreen';
